1acec8bd4122b390aa00c5fbe6d4741058853b34
IBeckzzzz <thiagobeck35@icloud.com>
2024-01-18 20:59:15 -0300

    Update

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,8 @@
 
-async function fetchProfileData() {
+/*async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/IBeckzzzz/portifolio-dio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
 }
+*/
